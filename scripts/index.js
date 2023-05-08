da245b1b3ea648866ff56626786424ddd777abf8
Aleksandr Bubnov <bubnov.a.s@mail.ru>
2023-05-08 18:52:56 +0300

    bug-fix + refactoring

---
+++ scripts/index.js
@@ -0,0 +1,44 @@
+const popupElement = document.querySelector('.popup');
+const popupCloseButtonElement = popupElement.querySelector('.close-button');
+const popupOpenButtonElement = document.querySelector('.edit-button')
+
+const popupInpitTitle = popupElement.querySelector('.popup__input-title')
+const popupInpitSubtitle = popupElement.querySelector('.popup__input-subtitle')
+const popupForm = popupElement.querySelector('.popup__form')
+
+let profileTitle = document.querySelector('.profile__title');
+let profileSubtitle = document.querySelector('.profile__subtitle');
+
+const openPopup = function () {
+  popupElement.classList.add('popup_opened');
+}
+
+const fillingPopupInputs = function () {
+  popupInpitTitle.value = profileTitle.textContent;
+  popupInpitSubtitle.value = profileSubtitle.textContent;
+}
+
+const closePopup = function () {
+  popupElement.classList.remove('popup_opened');
+}
+
+const closePopupByClickOnOverlay = function (event) {
+  if(event.currentTarget !== event.target) return;
+    closePopup();
+}
+
+const editTitles = function (event) {
+  profileTitle.textContent = popupInpitTitle.value;
+  profileSubtitle.textContent = popupInpitSubtitle.value;
+
+  event.preventDefault();
+  closePopup();
+}
+
+
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupOpenButtonElement.addEventListener('click', fillingPopupInputs);
+popupCloseButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOnOverlay);
+
+popupForm.addEventListener('submit', editTitles);
